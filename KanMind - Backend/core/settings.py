87b6947563e9af2bdbc0685d25b371c2193c7211
KonstantinFenzlein <kfenzlein@gmail.com>
2026-07-12 23:01:31 +0200

    update CORS

--- KanMind - Backend/core/settings.py
+++ KanMind - Backend/core/settings.py
@@ -137,7 +137,8 @@ REST_FRAMEWORK = {
 from corsheaders.defaults import default_headers
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",  
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
